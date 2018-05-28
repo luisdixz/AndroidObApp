103cc2cc280c7c7ba4da0688d4d899e19997744c
DESKTOP-8BPCFNC\luisdixz <luismainutd@gmail.com>
2018-05-28 09:21:05 -0500

    cree clase perfil

---
+++ app/src/main/java/com/diazmain/obapp/Perfil.kt
@@ -0,0 +1,12 @@
+package com.diazmain.obapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Perfil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
