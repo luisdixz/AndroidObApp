ec98aafdeb5cbff8bc2c0e43a95778f57c154cfd
DESKTOP-8BPCFNC\luisdixz <luismainutd@gmail.com>
2018-05-28 10:40:40 -0500

    Intento de programar en Kotlin

--- app/src/main/java/com/diazmain/obapp/MainActivity.kt
+++ app/src/main/java/com/diazmain/obapp/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.diazmain.obapp
 
+import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    fun goPerfil(view: View) {
 
     }
 }
